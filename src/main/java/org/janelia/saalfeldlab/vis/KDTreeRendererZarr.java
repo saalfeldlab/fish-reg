1b37da6c7579daf1c4e1431a62e26efd1b57a658
John Bogovic <bogovicj@janelia.hhmi.org>
2026-03-31 16:00:17 -0400

    feat: add render for points stored in zarr arrays

---
+++ src/main/java/org/janelia/saalfeldlab/vis/KDTreeRendererZarr.java
@@ -0,0 +1,408 @@
+package org.janelia.saalfeldlab.vis;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.DoubleUnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.janelia.saalfeldlab.n5.N5Reader;
+import org.janelia.saalfeldlab.n5.imglib2.N5Utils;
+import org.janelia.saalfeldlab.n5.universe.N5Factory;
+import org.janelia.saalfeldlab.n5.universe.StorageFormat;
+
+import bdv.util.BdvFunctions;
+import bdv.util.BdvOptions;
+import bdv.util.BdvStackSource;
+import ij.IJ;
+import ij.ImagePlus;
+import net.imglib2.FinalInterval;
+import net.imglib2.Interval;
+import net.imglib2.KDTree;
+import net.imglib2.RandomAccess;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.RealLocalizable;
+import net.imglib2.RealPoint;
+import net.imglib2.RealRandomAccessible;
+import net.imglib2.exception.ImgLibException;
+import net.imglib2.img.Img;
+import net.imglib2.img.imageplus.ImagePlusImgs;
+import net.imglib2.img.imageplus.ShortImagePlus;
+import net.imglib2.loops.LoopBuilder;
+import net.imglib2.parallel.TaskExecutors;
+import net.imglib2.realtransform.AffineTransform3D;
+import net.imglib2.realtransform.Scale;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.integer.UnsignedShortType;
+import net.imglib2.util.Intervals;
+import net.imglib2.view.Views;
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+
+public class KDTreeRendererZarr<T extends RealType<T>,P extends RealLocalizable> implements Runnable
+{
+
+	@Option(names = {"-i", "--input"}, required = true, description = "Input point ")
+	private String input;
+
+	@Option(names = {"-o", "--output"}, required = false, description = "Output image path")
+	private String output;
+
+	@Option(names = {"-p", "--psf-radius"}, required = true, description = "Radius for synapse point spread function")
+	private double radius;
+
+	@Option(names = {"-r", "--output-resolution"}, required = true, description = "Resolution of output image")
+	private String resolution;
+
+	@Option(names = {"-ir", "--input-resolution"}, required = false, description = "Resolution of input points")
+	private String inputResolution;
+	
+	@Option(names = {"-fz", "--flip-z"}, required = false, description = "Whether to flip the z dimension for each input path")
+	private String flipZArg;
+
+	@Option(names = {"-s", "--size"}, required = false, description = "Output image size")
+	private String sizeString;
+
+	@Option(names = {"-a", "--affine"}, required = false, description = "Affine applied to first set of points")
+	private String affineArg;
+
+	@Option(names = {"-q", "--nThreads"}, required = false, description = "Number of threads")
+	private int nThreads = 10;
+
+	private double searchDist;
+
+	private double searchDistSqr;
+
+	private double invSquareSearchDistance; 
+
+	private KDTree< T > tree;
+	
+	public static void main(String[] args) throws ImgLibException, IOException {
+
+		int exitCode = new CommandLine(new KDTreeRendererZarr()).execute(args);
+//		System.exit(exitCode);
+	}
+
+	public KDTreeRendererZarr() { }
+
+	public KDTreeRendererZarr( List<T> vals, List<P> pts, double searchDist )
+	{
+		tree = new KDTree< T >( vals, pts );
+		setSearchDist( searchDist );
+	}
+	
+	@Override
+	public void run() {
+
+		final BdvOptions opts = BdvOptions.options().numRenderingThreads(nThreads);
+		final boolean flipZ = flipZArg == null ? false : flipZArg.toLowerCase().equals("true");
+		if (output == null) {
+
+			System.out.println("display " + input);
+			System.out.println("flipZ " + flipZ);
+			System.out.println(" ");
+
+			BdvStackSource bdv = displayPoints(flipZ, opts);
+			opts.addTo(bdv);
+
+		} else {
+			final int i = 0;
+			final ImagePlus imp = copyToImagePlus(render(flipZ), nThreads);
+			IJ.save(imp, output);
+		}
+
+		System.out.println("done");
+	}
+
+	public RealRandomAccessible<T> getRealRandomAccessible(
+			final double searchDist,
+			final DoubleUnaryOperator rbf )
+	{
+		RBFInterpolator.RBFInterpolatorFactory< T > interp = 
+				new RBFInterpolator.RBFInterpolatorFactory< T >( 
+						rbf, searchDist, false,
+						tree.firstElement().copy() );
+
+		return Views.interpolate( tree, interp );
+	}
+	
+	public void setSearchDist( final double searchDist )
+	{
+		this.searchDist = searchDist;
+		searchDistSqr = searchDist * searchDist;
+		invSquareSearchDistance = 1.0 / (searchDist * searchDist); 
+	}
+
+	public static double rbf( final double rsqr, final double dSqr, 
+			final double invDSqr )
+	{
+		if (rsqr > dSqr)
+			return 0;
+		else
+			return 50 * (1 - (rsqr * invDSqr));
+	}
+
+	public BdvStackSource displayPoints(boolean flipZ, BdvOptions opts )
+	{
+		final AffineTransform3D affine = affineArg != null ? parseAffine(affineArg) : new AffineTransform3D();
+		final List<RealPoint> pts = loadZarr(input, 1, flipZ, affine);
+		final Interval itvl = boundingMaxInterval(pts);
+		System.out.println(Intervals.toString(itvl));
+		
+		final double[] res = Arrays.stream(resolution.split( "," ) ).mapToDouble( Double::parseDouble ).toArray();
+
+		final Scale inputScale;
+		if (inputResolution == null) {
+			inputScale = new Scale(new double[]{1, 1, 1});
+		} else {
+			final double[] ptres = Arrays.stream(inputResolution.split(","))
+					.mapToDouble(Double::parseDouble).toArray();
+			inputScale = new Scale(ptres);
+		}
+
+		final List< UnsignedShortType > vals = Stream.iterate( new UnsignedShortType( 1 ), x -> x ).limit( pts.size() )
+				.collect( Collectors.toList() );
+
+		System.out.println("radius: " + radius);
+		// build renderer
+		KDTreeRendererZarr< UnsignedShortType, RealPoint > treeRenderer = new KDTreeRendererZarr< UnsignedShortType, RealPoint >( 
+				vals, pts, radius );
+
+
+		final double dSqr = treeRenderer.searchDistSqr;
+		final double idSqr = treeRenderer.invSquareSearchDistance;
+		RealRandomAccessible< UnsignedShortType > source = treeRenderer.getRealRandomAccessible( 
+				radius, x -> rbf(x, dSqr, idSqr ));
+
+		final String name = input.substring(input.lastIndexOf('/'));
+		BdvStackSource<UnsignedShortType> bdv = BdvFunctions.show(source, itvl, name, opts);
+		bdv.setDisplayRange(0, 1);
+		
+		return bdv;
+	}
+	
+	public RandomAccessibleInterval render(boolean flipZ)
+	{
+		final List<RealPoint> pts = loadZarr(input, 1, flipZ);
+		final Interval itvl = boundingMaxInterval(pts);
+		System.out.println(Intervals.toString(itvl));
+		
+		final double[] res = Arrays.stream( resolution.split( "," ) ).mapToDouble( Double::parseDouble ).toArray();
+
+		final Scale inputScale;
+		if (inputResolution == null) {
+			inputScale = new Scale(new double[]{1, 1, 1});
+		} else {
+			final double[] ptres = Arrays.stream(inputResolution.split(","))
+					.mapToDouble(Double::parseDouble).toArray();
+			inputScale = new Scale(ptres);
+		}
+
+		final List< UnsignedShortType > vals = Stream.iterate( new UnsignedShortType( 1 ), x -> x ).limit( pts.size() )
+				.collect( Collectors.toList() );
+
+		System.out.println("radius: " + radius);
+		// build renderer
+		
+		KDTreeRendererZarr< UnsignedShortType, RealPoint > treeRenderer = new KDTreeRendererZarr< UnsignedShortType, RealPoint >( 
+				vals, pts, radius );
+
+		final double dSqr = treeRenderer.searchDistSqr;
+		final double idSqr = treeRenderer.invSquareSearchDistance;
+		RealRandomAccessible< UnsignedShortType > source = treeRenderer.getRealRandomAccessible( 
+				radius, x -> rbf(x, dSqr, idSqr ));
+
+		return Views.interval( Views.raster( source ), itvl );
+	}
+
+	public static Interval boundingMaxInterval(List<RealPoint> points) {
+
+		long[] max = new long[3];
+		Arrays.fill(max, Long.MIN_VALUE);
+		
+		for( RealPoint p : points ) {
+			
+			final double x = p.getDoublePosition(0);
+			final double y = p.getDoublePosition(1);
+			final double z = p.getDoublePosition(2);
+
+			if (x > max[0])
+				max[0] = (long)x;
+
+			if (y > max[1])
+				max[1] = (long)y;
+
+			if (z > max[2])
+				max[2] = (long)z;
+		}
+		
+		// increment so that the interval includes the max value
+		
+		max[0]++;
+		max[1]++;
+		max[2]++;
+
+		return new FinalInterval(max);
+	}
+	
+	public static Interval boundingInterval(List<RealPoint> points) {
+		
+		long[] min = new long[3];
+		Arrays.fill(min, Long.MAX_VALUE);
+
+		long[] max = new long[3];
+		Arrays.fill(max, Long.MIN_VALUE);
+		
+		for( RealPoint p : points ) {
+			
+			final double x = p.getDoublePosition(0);
+			final double y = p.getDoublePosition(1);
+			final double z = p.getDoublePosition(2);
+
+			if (x < min[0])
+				min[0] = (long)x;
+			
+			if (y < min[1])
+				min[1] = (long)y;
+
+			if (z < min[2])
+				min[2] = (long)z;
+
+
+			if (x > max[0])
+				max[0] = (long)x;
+
+			if (y > max[1])
+				max[1] = (long)y;
+
+			if (z > max[2])
+				max[2] = (long)z;
+		}
+
+		return new FinalInterval(min, max);
+	}
+
+	public static double[] strToDouble( final String[] s )
+	{
+		final double[] out = new double[ s.length ];
+		for( int i = 0; i < s.length; i++ )
+			out[ i ] = Double.parseDouble( s[ i ]);
+
+		return out;
+	}
+
+	static boolean[] parseBooleans( final String string )
+	{
+
+		if (string == null)
+			return new boolean[0];
+
+		final String[] split = string.split(",");
+		final boolean[] out = new boolean[split.length];
+		int i = 0;
+		for (String s : split) {
+			if (s.isEmpty() || s.equals("0"))
+				out[i++] = false;
+			else
+				out[i++] = true;
+		}
+
+		return out;
+	}
+	
+	static AffineTransform3D parseAffine(final String stringArg )
+	{
+
+		final String string;
+		final boolean inv = stringArg.startsWith("i");
+		if (inv)
+			string = stringArg.substring(1);
+		else
+			string = stringArg;
+
+		final AffineTransform3D affine = new AffineTransform3D();
+		if (string == null)
+			return affine;
+
+		final double[] params = Arrays.stream(string.split(",")).mapToDouble(Double::parseDouble).toArray();
+		affine.set(params);
+
+		if (inv)
+			return affine.inverse();
+		else
+			return affine;
+	}
+
+	final static int DIM = 0;
+	final static int IDX = 1;
+
+	public static <T extends RealType<T>> List<RealPoint> loadZarr( String path, int subsamplingFactor, boolean flipZ) {
+
+		// use identity affine
+		return loadZarr(path, subsamplingFactor, flipZ, new AffineTransform3D());
+	}
+
+	public static <T extends RealType<T>> List<RealPoint> loadZarr( String path, int subsamplingFactor, boolean flipZ, AffineTransform3D affine ) {
+
+		System.out.println("load with affine: " + affine);
+		final N5Reader zarr = new N5Factory().openReader(StorageFormat.ZARR, path);
+		
+		@SuppressWarnings("unchecked")
+		final Img<T> pointImg = (Img<T>)N5Utils.open(zarr, "");
+		final RandomAccess<T> ra = pointImg.randomAccess();
+
+		final int nd = (int)pointImg.dimension(DIM);
+		final int N = (int)pointImg.dimension(IDX);
+		System.out.println(Intervals.toString(pointImg));
+		
+		ArrayList<RealPoint> points = new ArrayList<>();
+		for( int i = 0; i < N; i+= subsamplingFactor ) {
+			final RealPoint pt = new RealPoint(getPoint(nd, i, flipZ, ra));
+			affine.apply(pt, pt);
+			points.add(pt);
+		}
+
+		return points;
+	}
+
+	private static <T extends RealType<T>>  double[] getPoint(int nd, int i, boolean flipZ, RandomAccess<T> ra) {
+
+		final double[] pt = new double[nd];
+		ra.setPosition(0, DIM);
+		ra.setPosition(i, IDX);
+
+		pt[0] = ra.get().getRealDouble();
+
+		ra.fwd(DIM);
+		pt[1] = ra.get().getRealDouble();
+
+		ra.fwd(DIM);
+		pt[2] = ra.get().getRealDouble();
+
+		if (flipZ)
+			pt[2] *= -1;
+
+		return pt;
+	}
+	
+	public static <T extends RealType<T>> ImagePlus copyToImagePlus( 
+			final RandomAccessibleInterval< T > img,
+			final int nThreads )
+	{
+		ShortImagePlus<UnsignedShortType> outImg = ImagePlusImgs.unsignedShorts(Intervals.dimensionsAsLongArray( img ));
+
+		LoopBuilder< BiConsumer< T, UnsignedShortType > > loop;
+		if( nThreads == 1)
+			loop = LoopBuilder.setImages( img, outImg );
+		else
+			loop = LoopBuilder.setImages( img, outImg ).multiThreaded( TaskExecutors.fixedThreadPool( nThreads ) );
+
+		loop.forEachPixel( (x,y) -> y.setReal( x.getRealDouble() ));
+		return outImg.getImagePlus();
+	}
+
+
+}
---
+++ src/main/java/org/janelia/saalfeldlab/vis/RBFInterpolator.java
@@ -0,0 +1,195 @@
+package org.janelia.saalfeldlab.vis;
+
+import java.util.function.DoubleUnaryOperator;
+
+import net.imglib2.KDTree;
+import net.imglib2.RealInterval;
+import net.imglib2.RealPoint;
+import net.imglib2.RealRandomAccess;
+import net.imglib2.RealRandomAccessible;
+import net.imglib2.Sampler;
+import net.imglib2.interpolation.InterpolatorFactory;
+import net.imglib2.neighborsearch.RadiusNeighborSearch;
+import net.imglib2.neighborsearch.RadiusNeighborSearchOnKDTree;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.view.Views;
+
+/**
+ * A radial basis function interpolator
+ * @author John Bogovic
+ *
+ * @param <T>
+ */
+public class RBFInterpolator<T extends RealType<T>> extends RealPoint implements RealRandomAccess< T >
+{
+	final static protected double minThreshold = Double.MIN_VALUE * 1000;
+
+	final protected RadiusNeighborSearch< T > search;
+	
+	final protected KDTree< T > tree;
+
+	final T value;
+	
+	protected double searchRadius;
+	
+	protected double threshold = 0;
+
+	final DoubleUnaryOperator rbf;  // from squaredDistance to weight
+	
+	final boolean normalize;
+	
+	// need to know the min/maxes of the block that the synapses came from
+
+
+	public RBFInterpolator(
+			final KDTree< T > tree, 
+			final DoubleUnaryOperator rbf, 
+			final double searchRadius,
+			final double threshold,
+			final boolean normalize,
+			T t )
+	{
+		super( tree.numDimensions() );
+
+		this.rbf = rbf;
+		this.tree = tree;
+		this.search = new RadiusNeighborSearchOnKDTree< T >( tree );
+		this.normalize = normalize;
+		this.searchRadius = searchRadius;
+		this.threshold = threshold;
+
+		this.value = t.copy();
+	}
+
+	public RBFInterpolator(
+			final KDTree< T > tree, 
+			final DoubleUnaryOperator rbf, 
+			final double searchRadius,
+			final boolean normalize,
+			T t )
+	{
+		this( tree, rbf, searchRadius, 0, normalize, t.copy() );
+	}
+	
+	public double getThreshold()
+	{
+		return this.threshold;
+	}
+
+	public void setThreshold( final double threshold )
+	{
+		this.threshold = threshold;
+	}
+
+	public double getRadius( )
+	{
+		return searchRadius;
+	}
+
+	public void setRadius( final double radius )
+	{
+		this.searchRadius = radius;
+	}
+
+	public void increaseRadius( final double amount )
+	{
+		this.searchRadius += amount;
+	}
+
+	public void decreaseRadius( final double amount )
+	{
+		this.searchRadius -= amount;
+	}
+
+	@Override
+	public T get()
+	{
+		search.search( this, searchRadius, false );
+
+		
+		// look if *this* position is within min/max interval of some hemi-brain block
+		
+		if ( search.numNeighbors() == 0 )
+			value.setZero();
+		else
+		{
+			double sumIntensity = 0;
+			double sumWeights = 0;
+
+			for ( int i = 0; i < search.numNeighbors(); ++i )
+			{
+				final Sampler< T > sampler = search.getSampler( i );
+
+				if ( sampler == null )
+					break;
+
+				final T t = sampler.get();
+				
+				if( t.getRealDouble() < threshold )
+					continue;
+
+				final double weight = rbf.applyAsDouble( search.getSquareDistance( i ) );
+
+				if( normalize )
+					sumWeights += weight;
+
+				sumIntensity += t.getRealDouble() * weight;
+			}
+
+			if( normalize )
+				value.setReal( sumIntensity / sumWeights );
+			else
+				value.setReal( sumIntensity );
+		}
+
+		return value;
+	}
+
+	@Override
+	public RBFInterpolator< T > copy()
+	{
+		return new RBFInterpolator< T >( tree,
+				rbf, searchRadius, threshold, normalize, value );
+	}
+
+	@Override
+	public RBFInterpolator< T > copyRealRandomAccess()
+	{
+		return copy();
+	}
+	
+
+	public static class RBFInterpolatorFactory<T extends RealType<T>> implements InterpolatorFactory< T, KDTree< T > >
+	{
+		final double searchRad;
+		final DoubleUnaryOperator rbf;
+		final boolean normalize;
+		T val;
+
+		public RBFInterpolatorFactory( 
+				final DoubleUnaryOperator rbf,
+				final double sr, 
+				final boolean normalize, 
+				T t )
+		{
+			this.searchRad = sr;
+			this.rbf = rbf;
+			this.normalize = normalize;
+			this.val = t;
+		}
+
+		@Override
+		public RBFInterpolator<T> create( final KDTree< T > tree )
+		{
+			return new RBFInterpolator<T>( tree, rbf, searchRad, false, val );
+		}
+
+		@Override
+		public RealRandomAccess< T > create( 
+				final KDTree< T > tree,
+				final RealInterval interval )
+		{
+			return create( tree );
+		}
+	}
+}
